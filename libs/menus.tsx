70e1032ff77a26703a32c226c0ec29fbadfbc699
viveksinra <vivekkrvks@gmail.com>
2024-03-09 17:58:33 +0530

    commented some menu for later

--- libs/menus.tsx
+++ libs/menus.tsx
@@ -7,63 +7,63 @@ export const menus = [
     label: "About Us",
     link: "/about/",
   },
-  {
-    label: "Pages",
-    link: "#",
-    submenu: [
+  // {
+  //   label: "Pages",
+  //   link: "#",
+  //   submenu: [
        
-      {
-        label: "Team",
-        link: "/team/",
-      },
-      {
-        label: "Features",
-        link: "/features/",
-      },
-      {
-        label: "Pricing",
-        link: "/pricing/",
-      },   
-      {
-        label: "Faq",
-        link: "/faq/",
-      },
-    ],
-  },
-  {
-    label: "Services",
-    link: "/services/",
-    submenu: [
-      {
-        label: "Services",
-        link: "/services/",
-      },
+  //     {
+  //       label: "Team",
+  //       link: "/team/",
+  //     },
+  //     {
+  //       label: "Features",
+  //       link: "/features/",
+  //     },
+  //     {
+  //       label: "Pricing",
+  //       link: "/pricing/",
+  //     },   
+  //     {
+  //       label: "Faq",
+  //       link: "/faq/",
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: "Services",
+  //   link: "/services/",
+  //   submenu: [
+  //     {
+  //       label: "Services",
+  //       link: "/services/",
+  //     },
      
-      {
-        label: "Service Details",
-        link: "/services/details/",
-      },
-    ],
-  },
-  {
-    label: "Partner",
-    link: "/partner/",
-  },
-  {
-    label: "Projects",
-    link: "/projects/",
-    submenu: [
-      {
-        label: "Projects",
-        link: "/projects/",
-      },
+  //     {
+  //       label: "Service Details",
+  //       link: "/services/details/",
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: "Partner",
+  //   link: "/partner/",
+  // },
+  // {
+  //   label: "Projects",
+  //   link: "/projects/",
+  //   submenu: [
+  //     {
+  //       label: "Projects",
+  //       link: "/projects/",
+  //     },
      
-      {
-        label: "Project Details",
-        link: "/projects/details/",
-      },
-    ],
-  },
+  //     {
+  //       label: "Project Details",
+  //       link: "/projects/details/",
+  //     },
+  //   ],
+  // },
  
   {
     label: "Contact",
