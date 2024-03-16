70633276eb3f48073983d2d5e2b9917721f734b8
viveksinra <vivekkrvks@gmail.com>
2024-03-17 02:34:46 +0530

    api removed

--- src/app/api/route.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    // Handle GET requests
-    if (req.method === "GET") {
-      res.status(200).json({ text: "Hello from Next.js 14 API!" });
-    } else {
-      // Handle other methods (optional)
-      res.status(405).end(); // Method Not Allowed
-    }
-  }
